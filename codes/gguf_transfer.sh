ebf3332c2b0acf78c0fdc83dda510b792e15dbd2
skihyeon <hero981001@gmail.com>
2024-03-05 13:54:53 +0900

    Implement Train preprocess

--- codes/gguf_transfer.sh
+++ codes/gguf_transfer.sh
@@ -1,10 +1,15 @@
-MODEL_PATH=$(jq -r '.model_path' ../configs/gguf_transfer_config.json)
-TRANSFER_NAME=$(jq -r '.transfer_model_name' ../configs/gguf_transfer_config.json)
-QUANTIZE_NAME=$(jq -r '.quantize_model_name' ../configs/gguf_transfer_config.json)
-QUANTIZE_OPTION=$(jq -r '.quantize_option' ../configs/gguf_transfer_config.json)
+# cd llama.cpp
 
-make LLAMA_OPENBLAS=1
+# pip install -r requirements.txt
+# make LLAMA_OPENBLAS=1
 
-python ../llama.cpp/convert.py "$MODEL_PATH"
+MODEL_PATH=$(jq -r '.model_path' /configs/gguf_transfer_config.json)
+TRANSFER_NAME=$(jq -r '.transfer_model_name' /configs/gguf_transfer_config.json)
+QUANTIZE_NAME=$(jq -r '.quantize_model_name' /configs/gguf_transfer_config.json)
+QUANTIZE_OPTION=$(jq -r '.quantize_option' /configs/gguf_transfer_config.json)
 
-llama.cpp/quantize "$MODEL_PATH/$TRANSFER_NAME" "$MODEL_PATH/$QUANTIZE_NAME" "$QUANTIZE_OPTION"
+# make LLAMA_OPENBLAS=1
+
+python /llama.cpp/convert.py "$MODEL_PATH"
+
+llama.cpp/quantize "$MODEL_PATH/$TRANSFER_NAME" "$MODEL_PATH/$QUANTIZE_NAME" "$QUANTIZE_OPTION"`
